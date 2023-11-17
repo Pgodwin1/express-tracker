d8fad390291a481da42a56e17887258e369ee7d2
Pgodwin1 <pgodwin287@gmail.com>
2023-11-17 13:59:55 +0100

    google signIn

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import {Auth} from './pages/auth/index'
-import { ExpenseTracker } from './pages/auth/expenses-tracker';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Auth } from "./pages/auth/index";
+import { ExpensesTracker } from "./pages/auth/expenses-tracker";
 
 function App() {
-
   return (
     <Router>
       <Routes>
-         <Route path='/' element={<Auth />} />
-         <Route path='expense-tracker' element={<ExpenseTracker />} />
+        <Route path="/" element={<Auth />} />
+        <Route path="expense-tracker" element={<ExpensesTracker />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/auth/expenses-tracker/index.jsx
+++ src/pages/auth/expenses-tracker/index.jsx
@@ -1,5 +1,3 @@
-export const ExpenseTracker = () => {
-    return(
-        <div>hello world Express tracker</div>
-    )
-}
+export const ExpensesTracker = () => {
+  return <div>hello world Express tracker</div>;
+};
