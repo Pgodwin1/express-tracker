aa7268944bae23042bc24d81dedfab781ed3bed1
Pgodwin1 <pgodwin287@gmail.com>
2023-11-17 14:52:54 +0100

    expense-tracker

---
+++ src/hooks/useAddTransaction.js
@@ -0,0 +1,18 @@
+import { addDoc, collection } from 'firebase/firestore';
+import { db } from '../config/firebase-config'
+export const useAddTransaction = () => {
+    const transactionCollectionRef = collection(db, "transactions")
+
+    const addTransaction = async() => {
+        await addDoc(transactionCollectionRef, {
+           userID: "",
+           description: "",
+           transactionAmount: 0,
+           transactionType: "",
+           createdAt: ""
+        });
+       
+    };
+
+    return{ addTransaction }
+}
